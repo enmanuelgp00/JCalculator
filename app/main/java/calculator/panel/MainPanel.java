fc8254abb5facd6160f0564ef719909452d2ecf0
unknown <enmanuelgutierrezpoey@gmail.com>
2025-11-24 21:46:58 -0500

    A bit of style, buttons rounds

--- app/main/java/calculator/panel/MainPanel.java
+++ app/main/java/calculator/panel/MainPanel.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;    
+import java.awt.geom.RoundRectangle2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -50,7 +52,72 @@ public class MainPanel extends JPanel {
 		gbc.gridwidth = 1;
 		
 		for ( String buttonstr : buttonStrs) {
-			JButton button = new JButton( buttonstr );
+			JButton button = new JButton( buttonstr ) {
+				int radiusCorners = 7;
+				{
+					setContentAreaFilled(false);
+					setFocusPainted(false);
+					setBorderPainted(false);
+					setOpaque(false);
+				}
+				@Override
+				public void paintComponent( Graphics g ) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+					
+					//fill the roundede rectangle
+					if ( getModel().isPressed()) {
+						g2.setColor( getBackground().darker());
+					} else if ( getModel().isRollover()) {
+						g2.setColor(getBackground().brighter());
+					} else {
+						g2.setColor(getBackground());
+					}
+					g2.fillRoundRect( 0, 0, getWidth(), getHeight(), /*cornersRadius*/radiusCorners, radiusCorners);
+					
+					super.paintComponent(g2);
+					g2.dispose();
+				}
+				/*
+				@Override
+				protected void paintBorder( Graphics g ) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+					g2.setColor(getForeground());
+					g2.drawRoundRect(0, 0, getWidth(), getHeight(), radiusCorners, radiusCorners );
+					g2.dispose();
+					
+				} 
+				
+				@Override
+				public boolean contains( int x, int y ) {
+					return new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), radiusCorners, radiusCorners ).contains( x, y );
+				}
+				*/
+				/*
+				ellise button
+				@Override
+				public void paintComponent( Graphics g ) {
+					if (getModel().isArmed()) {
+						g.setColor(Color.lightGray);
+					} else {
+						g.setColor(getBackground());
+					}
+					g.fillOval(0, 0, getSize().width - 1, getSize().height - 1 );
+					super.paintComponent(g);
+				}
+				@Override
+				public void paintBorder( Graphics g ) {
+					g.setColor( getForeground());
+					g.drawOval(0, 0, getSize().width - 1, getSize().height - 1);
+				}
+				@Override
+				public boolean contains( int x, int y) {
+					if (getBounds() == null ) return false;
+					return new Ellipse2D.Float(0, 0, getWidth(), getHeight()).contains(x , y);
+				}
+				*/
+			};
 			button.setFont( font );
 			button.addActionListener( new ActionListener() {
 				@Override
